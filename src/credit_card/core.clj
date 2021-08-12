539e2d99865ec6cf34981111266082a5c8edad18
Tiago Vidal <tiagod3p@gmail.com>
2021-08-12 19:00:19 -0300

    feat: handling data with datomic

--- src/credit_card/core.clj
+++ src/credit_card/core.clj
@@ -1,45 +1,73 @@
 (ns credit-card.core
   (:require [credit-card.logic.purchases :as logic.purchases]
-            [credit-card.logic.credit-card :as logic.credit-card]))
+            [credit-card.logic.credit-card :as logic.credit-card]
+            [credit-card.logic.utils :as logic.utils]
+            [credit-card.models.credit-card :as models.credit-card]
+            [credit-card.models.purchase :as models.purchase]
+            [credit-card.db.datomic.config :as db.config]
+            [credit-card.db.datomic.credit-card :as db.credit-card]
+            [datomic.api :as d]))
 
-(def client-data {:full-name "Tiago Vidal"
-                  :cpf       "999.999.999-99"
-                  :email     "tiago@my-email.com.br"
-                  :credit-cards []})
+(def client-data {:client/full-name    "Tiago Vidal"
+                  :client/cpf          "999.999.999-99"
+                  :client/email        "tiago@my-email.com.br"
+                  :client/credit-cards []})
 
-(def credit-card {:number          "111"
-                  :cvv             "222"
-                  :expiration-date (java.time.YearMonth/parse "2029-09")
-                  :limit           1000})
+(def credit-card {:credit-card/id              (logic.utils/uuid)
+                  :credit-card/number          "111"
+                  :credit-card/cvv             "222"
+                  :credit-card/expiration-date (java.time.YearMonth/parse "2029-09")
+                  :credit-card/limit           1000})
+
+(db.config/delete-db! db.config/db-uri)
+
+(def conn (db.config/open-connection! db.config/db-uri))
+
+(db.config/create-schema! conn (concat
+                                models.credit-card/datomic-schemas
+                                models.purchase/datomic-schemas))
+
+(db.credit-card/upsert-client-data! client-data conn)
+
+(db.credit-card/upsert-credit-card! credit-card conn)
+
+(let [all-credit-cards-datomic (db.credit-card/all-credit-cards (d/db conn))
+      first-credit-card        (first all-credit-cards-datomic)
+      credit-card-by-id        (db.credit-card/credit-card-by-id (:credit-card/id first-credit-card) (d/db conn))]
+  credit-card-by-id)
 
 (def purchase-1 (logic.purchases/purchase
-                   (java.time.LocalDate/parse "2021-05-07")
-                   199
-                   "iFood"
-                   "Restaurant"
-                   credit-card))
+                 (java.time.LocalDate/parse "2021-05-07")
+                 199
+                 "iFood"
+                 "Restaurant"
+                 credit-card))
 
 (def purchase-2 (logic.purchases/purchase
-                  (java.time.LocalDate/parse "2021-10-07")
-                  95
-                  "C&A"
-                  "Clothing"
-                  (:credit-card purchase-1)))
+                 (java.time.LocalDate/parse "2021-10-07")
+                 95
+                 "C&A"
+                 "Clothing"
+                 (:purchase/credit-card purchase-1)))
 
 (def purchase-3 (logic.purchases/purchase
-                  (java.time.LocalDate/parse "2021-10-07")
-                  100 
-                  "C&A"
-                  "Clothing"
-                  (:credit-card purchase-2)))
+                 (java.time.LocalDate/parse "2021-10-07")
+                 100
+                 "C&A"
+                 "Clothing"
+                 (:purchase/credit-card purchase-2)))
 
 (def all-purchases [purchase-1 purchase-2 purchase-3])
 
-(def approved-purchases (logic.purchases/search-purchases all-purchases {:approved? true}))
+(map #(db.credit-card/upsert-purchase-and-update-credit-card! % conn) all-purchases)
+
+(db.credit-card/credit-card-by-id (:credit-card/id credit-card) (d/db conn))
+
+(def approved-purchases (logic.purchases/search-purchases all-purchases {:purchase/approved? true}))
 
 (def client-data-with-initial-credit-card-associated (logic.credit-card/associate-credit-card-with-client credit-card client-data))
 
-(def credit-card-most-updated (:credit-card (last all-purchases)))
+(def credit-card-most-updated (:purchase/credit-card (last all-purchases)))
 
 (def client-data-updated (logic.credit-card/associate-credit-card-with-client
                           credit-card-most-updated
@@ -62,9 +90,9 @@
   (println "-------------------------------------------------")
   (println "Expenses by category:" (logic.purchases/group-purchases-by-category approved-purchases))
   (println "-------------------------------------------------")
-  (println "Search by merchant using query: {:merchant iFood}:" (logic.purchases/search-purchases all-purchases {:merchant "iFood"}))
+  (println "Search by merchant using query: {:merchant iFood}:" (logic.purchases/search-purchases all-purchases {:purchase/merchant "iFood"}))
   (println "-------------------------------------------------")
-  (println "Search by amount using query: {:amount 100}:" (logic.purchases/search-purchases all-purchases {:amount 100}))
+  (println "Search by amount using query: {:amount 100}:" (logic.purchases/search-purchases all-purchases {:purchase/amount 100}))
   (println "---------------------------------------------------")
   (println "Monthly bill of month 05" (logic.purchases/monthly-bill approved-purchases 2021 5))
   (println "-------------------------------------------------")
