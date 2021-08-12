a03663825fe07101d4032cfb29e2e2b376084970
Tiago Vidal <tiagod3p@gmail.com>
2021-08-12 18:59:07 -0300

    feat: add models to handle datomic schemas

--- src/credit_card/models/credit_card.clj
+++ src/credit_card/models/credit_card.clj
@@ -3,13 +3,50 @@
             [credit-card.models.general :as models.general]))
 
 (s/def CreditCard
-  {:number          s/Str
-   :cvv             s/Str
-   :expiration-date models.general/YearMonth
-   :limit           models.general/NumGreaterOrEqualThanZero})
+  {:credit-card/id              java.util.UUID
+   :credit-card/number          s/Str
+   :credit-card/cvv             s/Str
+   :credit-card/expiration-date models.general/YearMonth
+   :credit-card/limit           models.general/NumGreaterOrEqualThanZero})
 
-(def ClientData
-  {:full-name   s/Str
-   :cpf         s/Str
-   :email       s/Str
-   :credit-cards [CreditCard]})
+(s/def ClientData
+  {:client/full-name    s/Str
+   :client/cpf          s/Str
+   :client/email        s/Str
+   :client/credit-cards [CreditCard]})
+
+(def datomic-schemas [{:db/ident       :client/id
+                       :db/valueType   :db.type/uuid
+                       :db/cardinality :db.cardinality/one
+                       :db/unique      :db.unique/identity}
+                      {:db/ident       :client/full-name
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :client/cpf
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :client/email
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :client/credit-cards
+                       :db/valueType   :db.type/ref
+                       :db/cardinality :db.cardinality/many}
+
+                      {:db/ident       :credit-card/id
+                       :db/valueType   :db.type/uuid
+                       :db/cardinality :db.cardinality/one
+                       :db/unique      :db.unique/identity}
+                      {:db/ident       :credit-card/number
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :credit-card/cvv
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :credit-card/expiration-date
+                       :db/valueType   :db.type/instant
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :credit-card/limit
+                       :db/valueType   :db.type/float
+                       :db/cardinality :db.cardinality/one}])
+
+
--- src/credit_card/models/purchase.clj
+++ src/credit_card/models/purchase.clj
@@ -4,12 +4,35 @@
             [credit-card.models.credit-card :as models.credit-card]))
 
 (def Purchase
-  {:date        models.general/LocalDate
-   :amount      models.general/NumGreaterOrEqualThanZero
-   :merchant    s/Str
-   :category    s/Str
-   :approved?   s/Bool
-   :credit-card models.credit-card/CreditCard})
+  {:purchase/date        models.general/LocalDate
+   :purchase/amount      models.general/NumGreaterOrEqualThanZero
+   :purchase/merchant    s/Str
+   :purchase/category    s/Str
+   :purchase/approved?   s/Bool
+   :purchase/credit-card models.credit-card/CreditCard})
 
 (def CategoryAmount
   {s/Str models.general/NumGreaterOrEqualThanZero})
+
+(def datomic-schemas [{:db/ident       :purchase/id
+                       :db/valueType   :db.type/uuid
+                       :db/cardinality :db.cardinality/one
+                       :db/unique      :db.unique/identity}
+                      {:db/ident       :purchase/date
+                       :db/valueType   :db.type/instant
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :purchase/amount
+                       :db/valueType   :db.type/float
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :purchase/merchant
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :purchase/category
+                       :db/valueType   :db.type/string
+                       :db/cardinality :db.cardinality/one}
+                      {:db/ident       :purchase/approved?
+                       :db/valueType   :db.type/boolean
+                       :db/cardinality :db.cardinality/many}
+                      {:db/ident       :purchase/credit-card
+                       :db/valueType   :db.type/ref
+                       :db/cardinality :db.cardinality/one}])
