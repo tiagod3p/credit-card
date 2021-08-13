83c3f1b5c15ac701818ef3617d37e201f4d6a6ae
Tiago Vidal <tiagod3p@gmail.com>
2021-08-13 18:25:04 -0300

    feat: add new functions to convert date

--- src/credit_card/common/time.clj
+++ src/credit_card/common/time.clj
@@ -16,3 +16,15 @@
   [^java.time.YearMonth ym]
   (local-date->inst (java.time.LocalDate/of (.getYear ym) (.getMonth ym) 1)))
 
+(defn inst->local-date ^java.time.LocalDate
+  [inst]
+  (-> inst
+      .toInstant
+      (java.time.LocalDate/ofInstant java.time.ZoneOffset/UTC)))
+
+(defn inst->year-month ^java.time.YearMonth
+  [inst]
+  (-> inst
+      inst->local-date
+      java.time.YearMonth/from))
+
--- src/credit_card/common/utils.clj
+++ src/credit_card/common/utils.clj
@@ -1,5 +1,28 @@
-(ns credit-card.common.utils)
+(ns credit-card.common.utils
+  (:require [clojure.walk :as walk]))
 
 (defn uuid
   []
   (java.util.UUID/randomUUID))
+
+(defn add-uuid
+  ([m]
+   (if (get m :id)
+     m
+     (assoc m :id (uuid))))
+
+  ([m namespace]
+   (let [ns-keyword (keyword namespace "id")]
+     (if (get m ns-keyword)
+       m
+       (assoc m ns-keyword (uuid))))))
+
+(defn remove-db-id
+  [m]
+  (if (map? m)
+    (dissoc m :db/id)
+    m))
+
+(defn remove-all-db-ids
+  [map-or-coll]
+  (walk/prewalk remove-db-id map-or-coll))
